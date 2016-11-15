17883fbdffe8e3b67ea792d9e3d727c1bd43bea9
Yaniislav <yanikkcr7@gmail.com>
2016-11-15 23:38:29 +0200

    All classes were added to project

---
+++ WpfApplication1/WpfApplication1/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    enum Position
+    {
+      Assistant,
+      Docent,
+      GraduateStudent,
+      Professor,
+      Teacher,
+      SimpleCollaborator
+    }
+    enum FreeOrContract
+    {
+        Contract,
+        Free
+    }
+}
